04333806a31e584d431921870fe92b6947db7ab9
garuca <gabriel.rufino.carvalho@gmail.com>
2025-09-26 01:01:20 -0300

    first commit

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "br.com.garuca.code4all"
+    compileSdk = project.property("flutter.compileSdkVersion").toString().toInt()
+    ndkVersion = project.property("flutter.ndkVersion").toString()
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "br.com.garuca.code4all"
+        minSdk = project.property("flutter.minSdkVersion").toString().toInt()
+        targetSdk = project.property("flutter.targetSdkVersion").toString().toInt()
+        versionCode = flutter.versionCode.toInt()
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
